9749b260705c0b5517c485d4aa47c31d14ddc673
Eldad Assis <eldada@jfrog.com>
2020-06-10 17:52:51 +0300

    Improve findEmptyNamespaces.sh to use two calls to kubectl only

--- findEmptyNamespaces.sh
+++ findEmptyNamespaces.sh
@@ -3,10 +3,14 @@
 # Go over all namespaces in a cluster and check if they are empty of workloads and controller objects
 
 OBJECTS=pods,jobs,cronjobs,deployments,daemonsets,statefulsets
+NS_ALL=ns-all.txt
+NS_WL=ns-wl.txt
 
-echo "Empty namespaces:"
-for n in $(kubectl get ns --no-headers -o=custom-columns=NAME:.metadata.name); do
-    if [ -z "$(kubectl get ${OBJECTS} -n ${n} --no-headers 2> /dev/null)" ]; then
-        echo "${n}"
-    fi
-done
+# Get list of all namespaces
+kubectl get ns --no-headers -o=custom-columns=NAME:.metadata.name > ${NS_ALL}
+
+# Get list of all namespaces with any workload in them
+kubectl get ${OBJECTS} --no-headers --all-namespaces -o=custom-columns=NAMESPACE:.metadata.namespace | sort -u > ${NS_WL}
+
+# Use grep to find all values in NS_ALL that are not in NS_WL (these are empty namespaces)
+grep -vf ${NS_WL} ${NS_ALL}
