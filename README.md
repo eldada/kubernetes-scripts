192f3d88ea08d0dfb03a534481e08d204f4b383a
Eldad Assis <eldada@jfrog.com>
2021-03-09 17:25:54 +0200

    Refactor to a CSV format and add column for detected high load

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ $ <script> --help
 CSV format is very automation friendly and is great for pasting in an Excel or Google sheet for further processing.
 * [getRestartingPods.sh](getRestartingPods.sh): Get all pods (all or single namespace) that have restarts detected in one or more containers. Formatted in CSV.
 * [podReady.sh](podReady.sh): Simple script to check if pod is really ready. Check status is 'Running' and that all containers are ready. Returns 0 if ready. Returns 1 if not ready.
-* [getNodesLoad.sh](getNodesLoad.sh): Script to traverse over the `kube-proxy` pods to get the nodes load average and number tof CPUs.
+* [getNodesLoadCSV.sh](getNodesLoadCSV.sh): Traverse over the `kube-proxy` pods to get the nodes load average and number of CPUs in a CSV format.
 
 ## YAML
 * [podWithHostFS.yaml](yaml/podWithHostFS.yaml): A pod with the host root file system mounted into it.<br>
---
+++ getNodesLoadCSV.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+errorExit () {
+    echo -e "\nERROR: $1\n"
+    exit 1
+}
+
+# Check we have the kube-proxy daemonset so we can run commands on its pods
+kubectl get ns kube-system > /dev/null 2>&1 || errorExit "Namespace kube-system not found"
+kubectl get ds kube-proxy -n kube-system > /dev/null 2>&1 || errorExit "Daemonset kube-proxy not found in kube-system"
+
+pods=$(kubectl get po -n kube-system | grep kube-proxy | awk '{print $1}' | tr '\n' ' ')
+
+# Header for the CSV output
+echo "Node, Load 1 min, Load 5 min, Load 15 min, CPU, High load"
+
+# Go over the pods and extract data
+for p in $pods; do
+    alert="-"
+    node=$(kubectl describe po -n kube-system $p | grep Node: | awk '{print $2}')
+    line=$(kubectl exec -n kube-system $p -- sh -c "cat /proc/loadavg | awk '{print \$1 \" \" \$2 \" \" \$3 \" \"}' ; nproc" | tr -d '\n')
+    load1=$(echo $line | awk '{print $1}')
+    load5=$(echo $line | awk '{print $2}')
+    load15=$(echo $line | awk '{print $3}')
+    cpu=$(echo $line | awk '{print $4}')
+    load_int=$(echo "$load1" | sed 's/\..*//g')
+
+    # If load > cpu, it should be marked as "High load"
+    if [[ $load_int -gt $cpu ]]; then alert="YES"; fi
+
+    echo "$node, $load1, $load5, $load15, $cpu, $alert"
+done
+
