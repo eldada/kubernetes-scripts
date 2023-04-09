c1449360f7c244723b23411899c86a00437cc653
Eldad Assis <eldada@jfrog.com>
2023-04-09 15:22:07 +0300

    [Breaking change] Simplify code and reduce some options

--- getRestartingPods.sh
+++ getRestartingPods.sh
@@ -3,12 +3,9 @@
 # UNCOMMENT this line to enable debugging
 # set -xv
 
-## Get all pods with restarting containers and sum the values
+## Get all pods with restarting containers sorted as csv output
 
-OUT=restarting.csv
 NAMESPACE=--all-namespaces
-QUITE=false
-HEADERS=true
 SCRIPT_NAME=$0
 
 ######### Functions #########
@@ -26,16 +23,12 @@ ${SCRIPT_NAME} - Extract resource requests and limits in a Kubernetes cluster fo
 Usage: ${SCRIPT_NAME} <options>
 
 -n | --namespace <name>                : Namespace to analyse.    Default: --all-namespaces
--o | --output <name>                   : Output file.             Default: ${OUT}
--q | --quite                           : Don't output to screen.  Default: Output to screen
 -h | --help                            : Show this usage
---no-headers                           : Don't print headers line
 
 Examples:
 ========
 Get all:                                                  $ ${SCRIPT_NAME}
 Get for namespace foo:                                    $ ${SCRIPT_NAME} --namespace foo
-Get for namespace foo and use output file bar.csv :       $ ${SCRIPT_NAME} --namespace foo --output bar.csv
 
 END_USAGE
 
@@ -50,21 +43,8 @@ processOptions () {
                 NAMESPACE="--namespace $2"
                 shift 2
             ;;
-            -o | --output)
-                OUT=$2
-                shift 2
-            ;;
-            -q | --quite)
-                QUITE=true
-                shift 1
-            ;;
-            --no-headers)
-                HEADERS=false
-                shift 1
-            ;;
             -h | --help)
                 usage
-                exit 0
             ;;
             *)
                 usage
@@ -96,54 +76,44 @@ addValues () {
 
 getRestartingPods () {
     local data=
-    local namespace=
-    local pod=
+    local namespace_and_pod=
     local restarts=
     local restart_sum=
+    local out_temp=
+
+    out_temp=$(mktemp)
 
     data=$(kubectl get pod ${NAMESPACE} -o=jsonpath='{range .items[*]}{.metadata.namespace},{.metadata.name},{.status.containerStatuses[*].restartCount}{"\n"}')
-
-    # Backup OUT file if already exists
-    [ -f "${OUT}" ] && cp -f "${OUT}" "${OUT}.$(date +"%Y-%m-%d_%H:%M:%S")"
-
-    # Prepare header for output CSV
-    if [ "${HEADERS}" == true ]; then
-        echo "Namespace,Pod,Total restarts" > "${OUT}"
-    else
-        echo -n "" > "${OUT}"
-    fi
 
     OLD_IFS=${IFS}
     IFS=$'\n'
     for l in ${data}; do
-#        echo "Line: $l"
-
-        namespace=$(echo "${l}" | awk -F, '{print $1}')
-        pod=$(echo "${l}" | awk -F, '{print $2}')
+        namespace_and_pod=$(echo "${l}" | awk -F, '{print $1","$2}')
         restarts=$(echo "${l}" | awk -F, '{print $3}')
 
         # Add restarts only if values exist
         if [ -n "${restarts}" ]; then
             # Go over container restart and add them up
             restart_sum=$(addValues "${restarts}")
-#            echo "Sum: $restart_sum"
 
             if [ "${restart_sum}" -gt 0 ]; then
-                local final_line=${namespace},${pod},${restart_sum}
-                if [ "${QUITE}" == true ]; then
-                    echo "${final_line}" >> "${OUT}"
-                else
-                    echo "${final_line}" | tee -a "${OUT}"
-                fi
+                local final_line=${namespace_and_pod},${restart_sum}
+                echo "${final_line}" >> "${out_temp}"
             fi
         fi
     done
     IFS=${OLD_IFS}
+
+    # Print sorted output
+    echo "Namespace,Pod,Total restarts"
+    sort -t , -n -k 3 -r "${out_temp}"
+
+    # Cleanup
+    rm -f "${out_temp}"
 }
 
 main () {
     processOptions "$@"
-    [ "${QUITE}" == true ] || echo "Getting Kubernetes cluster pods restarts"
     testConnection
     getRestartingPods
 }
